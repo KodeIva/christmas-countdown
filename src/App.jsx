6ad5a0ce1cb0caced201825c858aaf3518e751b1
Iva <ivakaradjova@yahoo.co.uk>
2024-12-14 22:11:47 +0000

    <div> added to display daysLeft

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,11 @@ import { useState } from 'react'
 
 import './App.css'
 
+
+
 function App() {
+  const [date, setDate] = useState(new Date());
+  const [christmasDay, setChtistmasDay] = useState('25 December 2024')
   const months = [
   "January",
   "February",
@@ -18,18 +22,30 @@ function App() {
   "December",
 ];
  
-  let date = new Date()
   console.log(date);
   let day = date.getDate()
   let month = months[date.getMonth()]
   let year = date.getFullYear()
   let currentDay = `${day}  ${month}  ${year}`
+
+  
+  let daysLeft = 25 - day
+  console.log(daysLeft);
+  
+ 
+
   return (
     <>
     
       <p>
         {currentDay}
       </p>
+      <p>{christmasDay}</p>
+
+      <div>
+        <h2>Days Until Christmas</h2>
+        <p>{daysLeft}</p>
+      </div>
     </>
   )
 }
