4a50807fb42a402f5e0a8ff0472a39bcd771a6a2
Iva <ivakaradjova@yahoo.co.uk>
2024-12-14 21:33:40 +0000

    months array added to disply month with word

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,33 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+ 
   let date = new Date()
   console.log(date);
   let day = date.getDate()
-  let month = date.getMonth()
+  let month = months[date.getMonth()]
   let year = date.getFullYear()
-  let currentDay = `${day} / ${month} / ${year}`
+  let currentDay = `${day}  ${month}  ${year}`
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
+    
+      <p>
         {currentDay}
       </p>
     </>
