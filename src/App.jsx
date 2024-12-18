5ed59b460b799e6675fa5f029adf9a92a34d4bcd
Iva <ivakaradjova@yahoo.co.uk>
2024-12-18 20:26:26 +0000

    days left calculated

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
-
 import './App.css'
 
 
 
 function App() {
-  const [date, setDate] = useState(new Date());
-  const [christmasDay, setChtistmasDay] = useState('25 December 2024')
+  const [date, setDate] = useState(new Date().getTime());
+  console.log(date);
+  
+  const [christmasDay, setChtistmasDay] = useState(new Date('December 25,2024').getTime())
+  console.log(christmasDay);
+  
   const months = [
   "January",
   "February",
@@ -21,30 +24,33 @@ function App() {
   "November",
   "December",
 ];
- 
-  console.log(date);
-  let day = date.getDate()
-  let month = months[date.getMonth()]
-  let year = date.getFullYear()
+
+  let day = new Date().getDate()
+  let month = months[new Date().getMonth()]
+  let year = new Date().getFullYear()
   let currentDay = `${day}  ${month}  ${year}`
 
-  
-  let daysLeft = 25 - day
+ let daysLeft = christmasDay - date
   console.log(daysLeft);
-  
+ let difference = parseInt(daysLeft)
+ console.log(difference);
  
+  let daysToChLeft = Math.floor(daysLeft / (1000 * 60 * 60 * 24))
+  console.log(daysToChLeft);
+  
 
+  
   return (
     <>
     
       <p>
-        {currentDay}
+   {currentDay}
       </p>
       <p>{christmasDay}</p>
 
       <div>
-        <h2>Days Until Christmas</h2>
-        <p>{daysLeft}</p>
+        <h2>{daysToChLeft} Days Until Christmas</h2>
+        <p>{}</p>
       </div>
     </>
   )
