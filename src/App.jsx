fa0216c52558678b12da1e9ec09ab294d670c414
Iva <ivakaradjova@yahoo.co.uk>
2025-05-26 13:24:00 +0100

    'relative' and 'bg-center' added to tailwind styling, removed from styles{}

--- src/App.jsx
+++ src/App.jsx
@@ -75,7 +75,7 @@ useEffect(() => {
 
   return (
    
-    <div  className='main flex flex-col h-screen w-screen bg-cover bg-no-repeat' style={{ height: '100vh', width: '100%', position: 'relative' ,backgroundImage: `url(${images[index]})`}} >
+    <div  className='main flex flex-col h-screen w-screen bg-center bg-cover bg-no-repeat relative transition-all duration-1000 ease-in-out' style={{ backgroundImage: `url(${images[index]})`}} >
         <Snowfall snowflakeCount={800} />
   {isChristmas ?(<div className='christmas flex items-center  mt-32 w-[80%] h-[500px] border-3 border-gray-300'> <h1  className='text-red-500 text-6xl md:text-8xl font-bold italic'>Merry Christmas!!!</h1></div>)  
     
